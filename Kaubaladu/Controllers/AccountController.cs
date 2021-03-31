f68fc327ce9611d11501ffffd6bf1a4dfc763b62
mmeest <martin.maasik.001@mail.ee>
2021-03-31 18:15:11 +0300

    Add project files.

---
+++ Kaubaladu/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kaubaladu.Models;
+using System.Web.Security;
+
+namespace Kaubaladu.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new KaubaladuEntities())
+            {
+                bool isValid = context.Users.Any(x => x.Username == model.Username && x.Password == model.Password);
+                if(isValid)
+                {
+                    if(model.Username == "admin")
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Username, false);
+                        return RedirectToAction("Index", "Users");
+                    }
+                    else
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Username, false);
+                        return RedirectToAction("Index", "Goods");
+                    }
+                }
+
+                ModelState.AddModelError("","Invalid username and password");
+            }
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using(var context = new KaubaladuEntities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Kaubaladu/Controllers/GoodsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kaubaladu;
+
+namespace Kaubaladu.Controllers
+{
+    [Authorize]
+    public class GoodsController : Controller
+    {
+        private KaubaladuEntities db = new KaubaladuEntities();
+
+        // GET: Goods
+        public ActionResult Index()
+        {
+            return View(db.Goods.ToList());
+        }
+
+        // GET: Goods/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Good good = db.Goods.Find(id);
+            if (good == null)
+            {
+                return HttpNotFound();
+            }
+            return View(good);
+        }
+
+        // GET: Goods/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Goods/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Image,Name,Serial,Cathegory,Origin,Unit,Amount,Owner")] Good good)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Goods.Add(good);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(good);
+        }
+
+        // GET: Goods/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Good good = db.Goods.Find(id);
+            if (good == null)
+            {
+                return HttpNotFound();
+            }
+            return View(good);
+        }
+
+        // POST: Goods/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Image,Name,Serial,Cathegory,Origin,Unit,Amount,Owner")] Good good)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(good).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(good);
+        }
+
+        // GET: Goods/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Good good = db.Goods.Find(id);
+            if (good == null)
+            {
+                return HttpNotFound();
+            }
+            return View(good);
+        }
+
+        // POST: Goods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Good good = db.Goods.Find(id);
+            db.Goods.Remove(good);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
